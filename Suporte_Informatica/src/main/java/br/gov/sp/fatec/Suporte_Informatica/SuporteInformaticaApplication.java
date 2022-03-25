ef8d3484c477ed8572a889325fb6e7e85bf3fc2d
Roberta Abreu <robertaabreu77@gmail.com>
2022-03-25 20:31:13 -0300

    first commit

---
+++ Suporte_Informatica/src/main/java/br/gov/sp/fatec/Suporte_Informatica/SuporteInformaticaApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.Suporte_Informatica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuporteInformaticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuporteInformaticaApplication.class, args);
+	}
+
+}
---
+++ Suporte_Informatica/src/test/java/br/gov/sp/fatec/Suporte_Informatica/SuporteInformaticaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.Suporte_Informatica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuporteInformaticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
